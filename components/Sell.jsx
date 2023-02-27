daa1aed6f7eac545a7a852d344f9acded1b40cee
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2023-02-27 17:20:13 +0100

    second commit

--- components/Sell.jsx
+++ components/Sell.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 
-function Sell() {
-    const [active, setActive] = useState(true)
-    const checkActive = () => {
+const headings = ["Brainstorm", "Design", "Code", "Final touches"];
 
-    }
+function Sell() {
+  const [page, setPage] = useState(0);
+
   return (
     <section className="w-[92vw] md:w-[950px] flex flex-col gap-2 mx-auto my-12">
-        <div className="flex text-sm gap-3">
-            <p className={`${active ? "border-b border-purple-500": ""} cursor-pointer`}>Brainstorming</p>
-            <p>Desiging</p>
-            <p>Coding</p>
-            <p>Final touches</p>
-        </div>
-        <div className="w-full border h-96 grid place-items-center">
-            <h1>Video</h1>
-        </div>
+      <div className="flex text-sm gap-3">
+        {headings.map((text, i) => (
+          i === page ? <p key={i} onClick={() => setPage(i)} className="border-b border-purple-500 cursor-pointer">{text}</p> : <p key={i} className="cursor-pointer" onClick={() => setPage(i)}>{text}</p>
+        ))}
+        
+      </div>
+      <div className="w-full border h-96 grid place-items-center shadow-lg">
+        {headings.map((text, i) => (
+          i === page && <h1 key={i}>{text}</h1>
+        ))}
+      </div>
     </section>
   );
 }
